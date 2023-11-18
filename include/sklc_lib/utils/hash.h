45cce1e39a7d8599e3618743aa75197769dfb9f7
Demiyanit <demasokolov2000@gmail.com>
2023-11-18 18:15:16 +0300

    started creating hash function for hashmap

--- include/sklc_lib/utils/hash.h
+++ include/sklc_lib/utils/hash.h
@@ -4,6 +4,15 @@
 #include <sklc_lib/defines.h>
 #include <sklc_lib/string.h>
 
-u64 array_hash_ccp_u64(const char* data, u64 array_length);
+// Used for custom hashmap, you can use it if you want to
+u64 hash_array_ccp_u64(const char* data, u64 array_length);
+u64 hash_array_string_u64(string data, u64 array_length);
+u64 hash_array_custom_data_u64(void* data, u64 data_size, u64 array_length);
+
+u64 hash_ccp_u64(const char* data);
+u64 hash_string_u64(string data);
+u64 hash_custom_data_u64(void* data, u64 data_size);
+
+
 
 #endif//_SKLC_LIB_UTILS_HASH_H_
--- src/sklc_lib/utils/hash.c
+++ src/sklc_lib/utils/hash.c
@@ -1,1 +1,50 @@
 #include <sklc_lib/utils/hash.h>
+
+u64 hash_ccp_u64_internal(const char* data, u64 data_length) {
+    // hash stuff
+
+    return 0; // temp
+}
+
+#pragma region etc
+
+u64 hash_array_ccp_u64(const char* data, u64 array_length) {
+    u64 data_length = string_strlen(data);
+
+    u64 ret = hash_ccp_u64_internal(data, data_length);
+
+    ret = ret % array_length;
+
+    return ret;
+}
+
+u64 hash_array_string_u64(string data, u64 array_length) {
+    u64 ret = hash_ccp_u64_internal(string_c_str(data), data.length);
+
+    ret = ret % array_length;
+    
+    return ret;
+}
+
+u64 hash_array_custom_data_u64(void* data, u64 data_size, u64 array_length) {
+    u64 ret = hash_ccp_u64_internal(data_size, data);
+
+    ret = ret % array_length;
+    
+    return ret;
+}
+
+u64 hash_ccp_u64(const char* data) {
+    u64 data_length = string_strlen(data);
+    return hash_ccp_u64_internal(data, data_length);
+}
+
+u64 hash_string_u64(string data) {
+    return hash_ccp_u64_internal(string_c_str(data), data.length);
+}
+
+u64 hash_custom_data_u64(void* data, u64 data_size) {
+    return hash_ccp_u64_internal(data_size, data);
+}
+
+#pragma endregion etc
